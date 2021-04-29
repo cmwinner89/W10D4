9c667542ee1dd177420bb417762a932aab14c3aa
michelleroos <hello.michelleroos@gmail.com>
2021-04-29 15:07:50 -0400

    created the store

--- react_to_do/frontend/todo_redux.jsx
+++ react_to_do/frontend/todo_redux.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {receiveTodo, receiveTodos} from './actions/todo_actions'
+import configureStore from './store/store'
 
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById("root");
     const test = <h1>This is our test</h1>
     ReactDOM.render(test, root)
-    // window.store = store;
+    const store = configureStore();
+    window.store = store;
     window.receiveTodo = receiveTodo;
     window.receiveTodos = receiveTodos;
 })
