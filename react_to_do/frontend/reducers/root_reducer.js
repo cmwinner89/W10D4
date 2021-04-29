b884dbde6eca9e5a47f68369f0cf5908d2ea56f7
Collin Winner <cmwinner89@gmail.com>
2021-04-29 16:29:05 -0400

    work on phase 2

--- react_to_do/frontend/reducers/root_reducer.js
+++ react_to_do/frontend/reducers/root_reducer.js
@@ -2,8 +2,8 @@ import {combineReducers} from 'redux';
 import todosReducer from './todos_reducer'
 
 const rootReducer = combineReducers({
-    todos: todosReducer,
-    todo: todosReducer
+    todos: todosReducer
+    // todo: todosReducer
 })
 
 export default rootReducer;
